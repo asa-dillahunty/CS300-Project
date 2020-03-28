7566abf47fae04d23f61c15a2586ae91be85754e
Asa Dillahunty <stantondillahunty@gmail.com>
2020-03-28 04:04:53 -0500

    Updated PP and Worker
    Updated PassageProcessor.java
    - it now creates workers with their respecitve paths, and is ready to update orders on prefixs
    
    Updated Worker.java
    - fixed ugly tabbing
    - added an untested method to return a list of good words from a text file
    - added running bool, not sure this is the way to terminate a thread, TODO: research this

--- edu/cs300/PassageProcessor.java
+++ edu/cs300/PassageProcessor.java
@@ -1,19 +1,44 @@
 package edu.cs300;
 
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.io.*;
 
 public class PassageProcessor {
-	static String passagePath = "../../passages.txt";
+	private static String PASSAGES_PATHS = "../../passages.txt";
+	private static int BLOCK_QUEUE_SIZE = 10;
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		ArrayList<String> paths = new ArrayList<String>();
+		// ArrayBlockingQueue prefixRequestArray;
 		
 		try {
-			Scanner pass = new Scanner(new File(passagePath));
+			Scanner pass = new Scanner(new File(PASSAGES_PATHS));
 			while (pass.hasNextLine())
-				System.out.println(pass.nextLine());
+				paths.add(pass.nextLine());
 		}
-		catch(Exception e){}
+		catch(Exception e){return;}
+
+		Worker[] workers = new Worker[paths.size()];
+		for (int i=0;i<paths.size();i++) {
+			workers[i] = new Worker(paths.get(i), i+1, new ArrayBlockingQueue<String>(BLOCK_QUEUE_SIZE),new ArrayBlockingQueue<String>(BLOCK_QUEUE_SIZE));
+			workers[i].start();
+		}
+
+		// while prefixes exist
+		// Somehow get a prefix
+		String prefix = "con";
+		for (Worker slave : workers) {
+			try {
+				slave.prefixRequestArray.put(prefix);
+			} catch (Exception e) {System.out.println("Cannot put prefix \"" + prefix + "\" in worker("+slave.id+")'s array");}
+		}
+
+		// after all the prefixes
+		// kill the worker threads
+		for (Worker slave : workers) {
+			slave.running = false;
+		}
+		
 	}
 }
--- edu/cs300/Worker.java
+++ edu/cs300/Worker.java
@@ -1,41 +1,73 @@
 package edu.cs300;
 import CtCILibrary.*;
 import java.util.concurrent.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
 
-class Worker extends Thread{
+class Worker extends Thread {
+	Trie textTrieTree;
+	ArrayBlockingQueue<String> prefixRequestArray;
+	ArrayBlockingQueue<String> resultsOutputArray;
+	int id;
+	String passageName;
+	String passagePath;
+	Boolean running;
 
-  Trie textTrieTree;
-  ArrayBlockingQueue prefixRequestArray;
-  ArrayBlockingQueue resultsOutputArray;
-  int id;
-  String passageName;
+	public Worker(String path,int id,ArrayBlockingQueue<String> prefix, ArrayBlockingQueue<String> results){
+		// this.textTrieTree=new Trie(words);
+		this.textTrieTree = null;
+		this.prefixRequestArray=prefix;
+		this.resultsOutputArray=results;
+		this.id=id;
+		this.passageName="Passage-"+Integer.toString(id)+".txt";//put name of passage here
+		this.passagePath = path;
+		this.running = true;
+	}
 
-  public Worker(String[] words,int id,ArrayBlockingQueue prefix, ArrayBlockingQueue results){
-    this.textTrieTree=new Trie(words);
-    this.prefixRequestArray=prefix;
-    this.resultsOutputArray=results;
-    this.id=id;
-    this.passageName="Passage-"+Integer.toString(id)+".txt";//put name of passage here
-  }
+	public ArrayList<String> getWordList() {
+		ArrayList<String> goodWords = new ArrayList<String>();
+		String word;
+		Scanner passage;
 
-  public void run() {
-    System.out.println("Worker-"+this.id+" ("+this.passageName+") thread started ...");
-    //while (true){
-      try {
-        String prefix=(String)this.prefixRequestArray.take();
-        boolean found = this.textTrieTree.contains(prefix);
-        
-        if (!found){
-          //System.out.println("Worker-"+this.id+" "+req.requestID+":"+ prefix+" ==> not found ");
-          resultsOutputArray.put(passageName+":"+prefix+" not found");
-        } else{
-          //System.out.println("Worker-"+this.id+" "+req.requestID+":"+ prefix+" ==> "+word);
-          resultsOutputArray.put(passageName+":"+prefix+" found");
-        }
-      } catch(InterruptedException e){
-        System.out.println(e.getMessage());
-      }
-    //}
-  }
+		try {
+			passage = new Scanner(new File(this.passagePath));
+		} catch (Exception e) {
+			return goodWords;
+		}
+
+		passage.useDelimiter("[^a-zA-Z\'-]");
+
+		while (passage.hasNext()) {
+			word = passage.next();
+			if (word.length() < 3 || word.contains("\'") || word.contains("-")) continue;
+
+			goodWords.add(word);
+		}
+
+		return goodWords;
+	}
+
+	public void run() {
+		System.out.println("Worker-"+this.id+" ("+this.passageName+") thread started ...");
+		this.textTrieTree = new Trie(getWordList());
+
+		while (running){
+			try {
+				String prefix=(String)this.prefixRequestArray.take();
+				boolean found = this.textTrieTree.contains(prefix);
+				
+				if (!found){
+					//System.out.println("Worker-"+this.id+" "+req.requestID+":"+ prefix+" ==> not found ");
+					resultsOutputArray.put(passageName+":"+prefix+" not found");
+				} else{
+					//System.out.println("Worker-"+this.id+" "+req.requestID+":"+ prefix+" ==> "+word);
+					resultsOutputArray.put(passageName+":"+prefix+" found");
+				}
+			} catch(InterruptedException e){
+				System.out.println(e.getMessage());
+			}
+		}
+	}
 
 }
