bf58074d01e8b6ca649d59174e541ff0cedf951a
Asa Dillahunty <stantondillahunty@gmail.com>
2020-03-30 23:28:31 -0500

    Create SendMessage.java

---
+++ SendMessage.java
@@ -0,0 +1,7 @@
+import edu.cs300.*;
+
+public class SendMessage {
+    public static void main(String[] args) {
+        MessageJNI.writeLongestWordResponseMsg( 1, "and", 1, "Jane Austen","andulousa", 6, 1);
+    }
+}
